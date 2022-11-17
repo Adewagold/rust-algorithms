a394d7166ed78cf09cbae0ec70f361d0b475f6b9
Adewale Ayokanmi Adeleye <adewagold@gmail.com>
2022-11-17 12:16:29 +0400

    Greatest common divisor of two integers

--- src/solution.rs
+++ src/solution.rs
@@ -34,3 +34,17 @@ use std::collections::HashMap;
         }
         unreachable!()
     }
+
+    pub fn greatest_common_divisor(mut n:u64, mut m:u64) -> u64 {
+        assert!(n!=0 && m!=0);
+        while m != 0 {
+            if m < n {
+                let t = m;
+                m = n;
+                n = t;
+            }
+            
+            m = m % n;
+        }
+        n
+    }
